e4403c40921176f0357427ba0aab69f0a94d853f
Kristofer Nissbrandt <kristofer.nissbrandt@collectorbank.com>
2018-10-24 09:36:22 +0200

    Clean up

--- src/Bank.Users.Domain/User/State/UserState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bank.Users.Domain.User.State
-{
-    public class UserState
-    {
-        public long IssuerId { get; internal set; }
-
-        public string PasswordHash { get; internal set; }
-    }
-}
